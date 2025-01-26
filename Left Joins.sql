f8a8fa2569c4ad98db27e9a74e55975855b91245
Alazhar Alsalmani <92651154+alazhar98@users.noreply.github.com>
2025-01-26 14:19:44 +0400

    Add files via upload

--- Left Joins.sql
+++ Left Joins.sql
@@ -11,7 +11,7 @@ select p.project_name
 from projects p
 left join departments d 
 on p.department_id = d.department_id
-where d.department_id=null ;
+where d.department_id is null ;
 
 /*List Departments Without Employees*/
 
@@ -19,4 +19,4 @@ select d.department_name
 from departments d 
 left join employees e 
 on d.department_id = e.department_id
-where e.employee_id=null;
+where e.employee_id is null;
